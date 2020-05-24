5af822665a99b6d00bfde62abe78a7de0a98b224
Febryardi13 <febryardiwork@gmail.com>
2020-05-24 16:11:56 +0700

    add cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require("cors")
 
 //Import connect mongoose
 const mongoose = require("mongoose");
@@ -35,6 +36,7 @@ app.use(session({
   saveUninitialized: true,
   cookie: { maxAge: 600000 }
 }));
+app.use(cors())
 app.use(flash());
 app.use(logger('dev'));
 app.use(express.json());
--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,7 +1,6 @@
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-// import uuid from "uuid/v4";
 
 const storageMultiple = multer.diskStorage({
   destination: function (req, file, cb) {
